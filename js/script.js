72731556155853a4dc754b40fb30b6270737cea2
Ahmed Adel <89941547+AhmedKazanova@users.noreply.github.com>
2021-12-23 18:43:47 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ window.onscroll = () =>{
    let value = window.scrollY / 10;
    document.querySelector('.home .mountain').style.bottom = `${-value}%`;
    document.querySelector('.home .content').style.bottom = `${value}%`;
-   document.querySelector('.home .moon').style.marginTop = `${-value}%`;
+   document.querySelector('.home .moon').style.marginTop = `${-value + 5}%`;
    document.querySelector('.home .cloud-1').style.right = `${-value}%`;
    document.querySelector('.home .cloud-2').style.left = `${-value}%`;
 };
